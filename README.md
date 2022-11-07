ac84da3cc74bfe152f721e64100d6bae9a444bc7
annwyl21 <ellen.a.ash@gmail.com>
2022-11-07 17:04:37 +0000

    add minimal steps puzzle solved

--- README.md
+++ README.md
@@ -12,3 +12,4 @@ A collection of puzzles that I have solved in python, from various sources inclu
 |Max Product of 3|[max_product_of_three.py](#/blob/main/max_product_of_three.py)|
 |Permutation|[permutation.py](#/blob/main/permutation.py)|
 |Missing element|[dictionary_range.py](#/permutation.py)
+|Minimum Steps|[frog.py](#/frog.py)
--- frog.py
+++ frog.py
@@ -1,3 +1,14 @@
+# Codility
+# Count minimal number of steps from x to y
+# x - start, y - end d - distance
+
+# A small frog wants to get to the other side of the road. 
+# The frog is currently located at position X and wants 
+# to get to a position greater than or equal to Y. 
+# The small frog always jumps a fixed distance, D.
+# Count the minimal number of jumps that 
+# the small frog must perform to reach its target.
+
 import math
 
 def solution(x, y, d):
@@ -5,7 +16,8 @@ def solution(x, y, d):
     return result
 
 print(solution(10, 85, 30))
-# x - start, y - end d - distance
+
+# Solution minus start point (x) from end point (y) and divide that by the measurement of each step (d).
 
 def solution1(x,y,d):
     remainder = (y-x)%d
@@ -15,3 +27,5 @@ def solution1(x,y,d):
     return int(result)
 
 print(solution1(10, 85, 30))
+
+# Solution uses the solution above but accounts for non-integer results and handles that situation.
