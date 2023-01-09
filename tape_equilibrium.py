b9f0db27d5b504dcca5c2b612cc76bf526005539
annwyl21 <ellen.a.ash@gmail.com>
2023-01-09 17:35:23 +0000

    running total doesn't work, back to original

--- tape_equilibrium.py
+++ tape_equilibrium.py
@@ -3,31 +3,48 @@
 
 def solution(A):
     minimum_difference = float('inf')
+    total = sum(A)
     for number in range(1, len(A)):
         x = sum(A[0:number])
-        diff = abs((sum(A) - x) - x)
+        diff = abs((total - x) - x)
         if diff < minimum_difference:
             minimum_difference = diff
-            if minimum_difference == 0:
-                return minimum_difference
+    if minimum_difference == 0:
+        return minimum_difference
     return 'minimum difference is ', minimum_difference
 
-#print(solution([3,1,2,4,3]))
-#print(solution([1000, -1000]))
+print(solution([3,1,2,-4,3]))
+print(solution([1000, -1000]))
+print(solution([-2,-3,-4,-1]))
+
+# running total solution is too complicated and doesn't work in all situations but working solution above fails on processing time
 
 def solutionplus(A):
+    sumofarray = 0
+    for item in A:
+        sumofarray += abs(item)
+    print(A, sum(A), sumofarray)
+    
     minimum_difference = float('inf')
+    
     running_total = 0
-    for number in range(1, len(A)):
-        running_total += A[number]
-        #print(x)
-        diff = abs((sum(A) - running_total) - running_total)
-        #print(diff)
+    for number in range(0, len(A)):
+        print(A[number])
+        running_total + A[number]
+        print('running total', running_total)
+        posInt = abs(running_total)
+        #diff = (sum(A) - running_total) - running_total
+        
+        firstslice = posInt
+        secondslice = sumofarray - posInt
+        
+        diff = abs(firstslice - secondslice)
+        print('firstslice', firstslice, ' - secondslice', secondslice, ' = diff', diff)
         if diff < minimum_difference:
             minimum_difference = diff
-        if minimum_difference == 0:
-            return minimum_difference
+
     return 'minimum difference is ', minimum_difference
 
-print(solutionplus([3,1,2,4,3]))
-print(solutionplus([1000, -1000]))
+#print(solutionplus([3,1,2,4,3]))
+#print(solutionplus([1000, -1000]))
+#print(solutionplus([-2,-3,-4,-1]))
