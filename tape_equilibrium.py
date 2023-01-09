41fd4df35b538b9747430790a96cda0534afcc12
annwyl21 <ellen.a.ash@gmail.com>
2023-01-09 13:31:04 +0000

    trying to reduce number of loops using sum

--- tape_equilibrium.py
+++ tape_equilibrium.py
@@ -3,27 +3,43 @@
 
 def solution(A):
     min_diff = float('inf')
-    sumofarray = 0
-    for element in A:
-        sumofarray += element
-    #print(sumofarray)
     smallsum = 0
+    bigsum = 0
     diff = 0
-    length = len(A)
-    indexlist = range(1, length)
+    length = len(A)+1
+    indexlist = range(0, length)
     for number in indexlist:
         breakpoint = number
         initialsection = A[0:breakpoint]
         #print(initialsection)
+        finalsection = A[breakpoint:]
+        #print(finalsection)
         for item in initialsection:
             smallsum += item
-            #print(smallsum)
-        diff = sumofarray - smallsum
-        #print(diff)
-        #print(min_diff)
+        for digit in finalsection:
+            bigsum += digit
+        diff = abs(bigsum - smallsum)
         smallsum = 0
+        bigsum = 0
         if diff < min_diff:
             min_diff = diff
     return 'minimum difference', min_diff
 
 print(solution([3,1,2,4,3]))
+
+def solutionplus(A):
+    minimum_difference = float('inf')
+    x = 0
+    y = sum(A) - x
+    indexlist = range(1, len(A))
+    for number in indexlist:
+        initialsection = A[0:number]
+        print
+        x = sum(initialsection)    
+        diff = y - x
+        if diff < minimum_difference:
+            minimum_difference = diff
+    return 'minimum difference is ', minimum_difference
+
+print(solutionplus([3,1,2,4,3]))
+
