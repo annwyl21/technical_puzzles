e4e1835f7470ce15bf1f94dc97b905573eac0d51
annwyl21 <ellen.a.ash@gmail.com>
2023-01-09 15:06:00 +0000

    using a running total instead of a slice

--- tape_equilibrium.py
+++ tape_equilibrium.py
@@ -5,12 +5,29 @@ def solution(A):
     minimum_difference = float('inf')
     for number in range(1, len(A)):
         x = sum(A[0:number])
-        y = sum(A) - x   
-        diff = abs(y - x)
+        diff = abs((sum(A) - x) - x)
         if diff < minimum_difference:
             minimum_difference = diff
+            if minimum_difference == 0:
+                return minimum_difference
     return 'minimum difference is ', minimum_difference
 
-print(solution([3,1,2,4,3]))
-print(solution([1000, -1000]))
+#print(solution([3,1,2,4,3]))
+#print(solution([1000, -1000]))
 
+def solutionplus(A):
+    minimum_difference = float('inf')
+    running_total = 0
+    for number in range(1, len(A)):
+        running_total += A[number]
+        #print(x)
+        diff = abs((sum(A) - running_total) - running_total)
+        #print(diff)
+        if diff < minimum_difference:
+            minimum_difference = diff
+        if minimum_difference == 0:
+            return minimum_difference
+    return 'minimum difference is ', minimum_difference
+
+print(solutionplus([3,1,2,4,3]))
+print(solutionplus([1000, -1000]))
